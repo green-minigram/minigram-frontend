cf415f64c5c955de1918830d2e349822dc13ec4b
jjack-1 <qazwsx0088@gmail.com>
2025-09-04 13:50:07 +0900

    [feat] 검색 화면 UI (#15)

---
+++ lib/ui/pages/holder/search/widgets/post_grid_sliver.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:minigram/_core/styles/m_size.dart';
+import 'package:minigram/ui/pages/holder/search/widgets/grid_item.dart';
+
+class PostGridSliver extends StatelessWidget {
+  const PostGridSliver({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => GridItem(),
+        childCount: 30,
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: MSize.kRatio.aspect34,
+        crossAxisSpacing: MSize.kGap.xxxxs,
+        mainAxisSpacing: MSize.kGap.xxxxs,
+      ),
+    );
+  }
+}
