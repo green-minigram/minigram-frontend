ece5cca76a4cd295c568f216f9076ee7bfa5eb74
hahamik <dlapdl0615@gmail.com>
2025-09-08 19:06:43 +0900

    1

--- lib/ui/pages/story/write/widget/story_write_body.dart
+++ lib/ui/pages/story/write/widget/story_write_body.dart
@@ -1,35 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:minigram/ui/pages/story/widgets/story_video_player.dart';
+import 'package:minigram/ui/pages/story/write/widget/story_preview.dart';
 
 class StoryWriteBody extends StatelessWidget {
+  final String videoPath;
+
   const StoryWriteBody({
     super.key,
     required this.videoPath,
   });
 
-  final String videoPath;
-
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
       children: [
-        Center(child: StoryVideoPlayer(videoPath: videoPath)), // 공통 사용
-        Positioned(
-          top: 40,
-          left: 16,
-          child: IconButton(
-            icon: Icon(Icons.close, color: Colors.red, size: 30),
-            onPressed: () => Navigator.pop(context),
-          ),
-        ),
-        Positioned(
-          bottom: 40,
-          right: 16,
-          child: FloatingActionButton(
-            onPressed: () => print("스토리 업로드 진행"),
-            child: const Icon(Icons.arrow_forward),
-          ),
-        ),
+        StoryPreview(videoPath: videoPath),
       ],
     );
   }
