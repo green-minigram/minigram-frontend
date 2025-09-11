a28edffc4d15a4c60c141778b348b6ce08cc1b3e
jjack-1 <qazwsx0088@gmail.com>
2025-09-11 11:24:47 +0900

    [feat] 로그인 기능 구현 (#34)
    - 로그인 더미 사용
    - 이메일 검증에 스페이스바(공백) 금지
    - 비밀번호 검증에 스페이스바(공백) 금지

--- lib/_core/util/validator_util.dart
+++ lib/_core/util/validator_util.dart
@@ -39,11 +39,15 @@ String validateUsername(String value) {
 }
 
 String validatePassword(String value) {
-  if (value.isEmpty) {
-    return "패스워드 공백이 들어갈 수 없습니다.";
-  } else if (value.length > 12) {
+  final v = value.trim();
+
+  if (v.isEmpty) {
+    return "패스워드에 공백만 입력할 수 없습니다.";
+  } else if (v.contains(' ')) {
+    return "패스워드에 공백이 들어갈 수 없습니다.";
+  } else if (v.length > 20) {
     return "패스워드의 길이를 초과하였습니다.";
-  } else if (value.length < 4) {
+  } else if (v.length < 4) {
     return "패스워드의 최소 길이는 4자입니다.";
   } else {
     return "";
@@ -61,9 +65,13 @@ String validatePasswordConfirm(String password, String passwordConfirm) {
 }
 
 String validateEmail(String value) {
-  if (value.isEmpty) {
-    return "이메일은 공백이 들어갈 수 없습니다.";
-  } else if (!isEmail(value)) {
+  final v = value.trim();
+
+  if (v.trim().isEmpty) {
+    return "이메일은 공백만 입력할 수 없습니다.";
+  } else if (v.contains(' ')) {
+    return "이메일에 공백이 들어갈 수 없습니다.";
+  } else if (!isEmail(v)) {
     return "이메일 형식에 맞지 않습니다.";
   } else {
     return "";
--- lib/ui/pages/auth/login_page/widgets/login_body.dart
+++ lib/ui/pages/auth/login_page/widgets/login_body.dart
@@ -2,26 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:minigram/_core/styles/m_color.dart';
 import 'package:minigram/_core/styles/m_size.dart';
 import 'package:minigram/m_route.dart';
-import 'package:minigram/ui/widgets/m_auth_text_form_field.dart';
+import 'package:minigram/ui/pages/auth/login_page/widgets/custom_logo.dart';
+import 'package:minigram/ui/pages/auth/login_page/widgets/login_form.dart';
 import 'package:minigram/ui/widgets/m_button.dart';
 
 class LoginBody extends StatelessWidget {
-  final scroll = ScrollController();
-
-  // MAuthTextFormField의 onTap 이벤트를 위한 함수
-  Future<void> _onFieldTapped() async {
-    await Future.delayed(const Duration(milliseconds: 600));
-    scroll.jumpTo(scroll.position.maxScrollExtent);
-  }
-
-  // 로그인 버튼의 onPressed 이벤트를 위한 함수
-  void _onLoginPressed(context) {
-    Navigator.pushNamedAndRemoveUntil(context, MRoute.mainHolder, (route) => route.isFirst);
-  }
+  final scrollController = ScrollController();
 
   // 새 계정 만들기 버튼의 onPressed 이벤트를 위한 함수
   void _onJoinPressed(BuildContext context) {
     Navigator.pushNamed(context, MRoute.joinEmail);
+  }
+
+  // MAuthTextFormField의 onTap 이벤트를 위한 함수
+  Future<void> _onFieldTapped() async {
+    await Future.delayed(const Duration(milliseconds: 600));
+    scrollController.jumpTo(scrollController.position.maxScrollExtent);
   }
 
   @override
@@ -31,46 +27,24 @@ class LoginBody extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Flexible(
-            child: Form(
-              child: ListView(
-                controller: scroll,
-                children: [
-                  Container(
-                    color: Colors.black26,
-                    height: 300,
-                  ),
-                  SizedBox(height: MSize.kGap.l),
-                  MAuthTextFormField(
-                    hintText: "이메일",
-                    keyboardType: TextInputType.emailAddress,
-                    onTap: _onFieldTapped,
-                  ),
-                  SizedBox(height: MSize.kGap.l),
-                  MAuthTextFormField(
-                    hintText: "비밀번호",
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: true,
-                    onTap: _onFieldTapped,
-                  ),
-                  SizedBox(height: MSize.kGap.l),
-                  MButton(
-                    onPressed: () {
-                      _onLoginPressed(context);
-                    },
-                    text: "로그인",
-                  ),
-                  SizedBox(height: MSize.kGap.l),
-                ],
-              ),
+          Expanded(
+            child: ListView(
+              controller: scrollController,
+              children: [
+                CustomLogo(),
+                SizedBox(height: MSize.kGap.l),
+                LoginForm(onFieldTapped: _onFieldTapped),
+              ],
             ),
           ),
           MButton.outline(
-            onPressed: () => _onJoinPressed(context),
             text: "새 계정 만들기",
             textColor: MColor.kText.blue,
             borderSide: BorderSide(color: MColor.kLine.main),
             backgroundColor: MColor.kBackGround.normal,
+            onPressed: () {
+              _onJoinPressed(context);
+            },
           ),
           SizedBox(height: MSize.kGap.doubleHuge),
         ],
