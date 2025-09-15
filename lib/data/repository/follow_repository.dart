5312624d92a0f8cfc28fb9f38c6ba392eec825ec
hahamik <dlapdl0615@gmail.com>
2025-09-15 12:42:42 +0900

    [feat] 팔로우 목록 통신

--- lib/data/repository/follow_repository.dart
+++ lib/data/repository/follow_repository.dart
@@ -3,54 +3,49 @@ import 'package:logger/logger.dart';
 class FollowRepository {
   // 팔로워 목록 조회
   Future<Map<String, dynamic>> getFollowers(int userId) async {
-    try {
-      // final response = await dio.get("/s/api/users/$userId/followers");
-      // final responseBody = response.data;
-      final responseBody = {
-        "status": 200,
-        "msg": "성공",
-        "body": {
-          "userList": [
-            {
-              "userId": 8,
-              "username": "luna",
-              "name": "루나",
-              "profileImageUrl": null,
-              "isFollowing": true,
-              "isMe": false,
-            },
-            {
-              "userId": 9,
-              "username": "zero",
-              "name": "제로",
-              "profileImageUrl": "https://picsum.photos/seed/zero/200",
-              "isFollowing": false,
-              "isMe": false,
-            },
-            {
-              "userId": 7,
-              "username": "neo",
-              "name": "네오",
-              "profileImageUrl": null,
-              "isFollowing": false,
-              "isMe": false,
-            },
-            {
-              "userId": 2,
-              "username": "ssar",
-              "name": "쌀",
-              "profileImageUrl": "https://picsum.photos/seed/ssar/200",
-              "isFollowing": false,
-              "isMe": true,
-            },
-          ],
-        },
-      };
-      return responseBody;
-    } catch (e) {
-      Logger().e("getFollowers 실패: $e");
-      return {"status": 500, "msg": "통신 실패"};
-    }
+    // final response = await dio.get("/s/api/users/$userId/followers");
+    // final responseBody = response.data;
+    final responseBody = {
+      "status": 200,
+      "msg": "성공",
+      "body": {
+        "userList": [
+          {
+            "userId": 8,
+            "username": "luna",
+            "name": "루나",
+            "profileImageUrl": null,
+            "isFollowing": true,
+            "isMe": false,
+          },
+          {
+            "userId": 9,
+            "username": "zero",
+            "name": "제로",
+            "profileImageUrl": "https://picsum.photos/seed/zero/200",
+            "isFollowing": false,
+            "isMe": false,
+          },
+          {
+            "userId": 7,
+            "username": "neo",
+            "name": "네오",
+            "profileImageUrl": null,
+            "isFollowing": false,
+            "isMe": false,
+          },
+          {
+            "userId": 2,
+            "username": "ssar",
+            "name": "쌀",
+            "profileImageUrl": "https://picsum.photos/seed/ssar/200",
+            "isFollowing": false,
+            "isMe": true,
+          },
+        ],
+      },
+    };
+    return responseBody;
   }
 
   // 팔로잉 목록 조회
