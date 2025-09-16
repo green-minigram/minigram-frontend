471bc2f54cc0c13f9bc5fe6753c631db49951470
jjack-1 <qazwsx0088@gmail.com>
2025-09-16 12:37:53 +0900

    .

--- lib/ui/pages/holder/profile/widgets/profile_body.dart
+++ lib/ui/pages/holder/profile/widgets/profile_body.dart
@@ -13,48 +13,90 @@ class ProfileBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NestedScrollView(
-      headerSliverBuilder: (context, innerBoxIsScrolled) {
-        return [
-          SliverPadding(
-            padding: EdgeInsets.symmetric(horizontal: MSize.kGap.l),
-            sliver: SliverList(
-              delegate: SliverChildListDelegate([
-                ProfileHeaderInfo(profileModel: profileModel),
-                ProfileHeaderBio(profileModel: profileModel),
-                ProfileHeaderButton(profileModel: profileModel),
-              ]),
-            ),
-          ),
-        ];
-      },
-      body: DefaultTabController(
-        length: 2,
-        child: Column(
-          children: [
-            TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.grid_on_rounded)), // 게시글
-                Tab(icon: Icon(Icons.video_library_rounded)), // 스토리
-              ],
-            ),
-            Expanded(
-              child: TabBarView(
-                children: [
-                  ProfileGridBuilder(
-                    isStoryTab: false,
-                    profileModel: profileModel,
-                  ), // 게시글 탭
-                  ProfileGridBuilder(
-                    isStoryTab: true,
-                    profileModel: profileModel,
-                  ), // 스토리 탭
-                ],
+    return DefaultTabController(
+      length: 2,
+      child: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverOverlapAbsorber(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              sliver: SliverPadding(
+                padding: EdgeInsets.symmetric(horizontal: MSize.kGap.l),
+                sliver: SliverList(
+                  delegate: SliverChildListDelegate([
+                    ProfileHeaderInfo(profileModel: profileModel),
+                    ProfileHeaderBio(profileModel: profileModel),
+                    ProfileHeaderButton(profileModel: profileModel),
+                  ]),
+                ),
               ),
+            ),
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: _TabBarDelegate(
+                TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.grid_on_rounded)), // 게시글
+                    Tab(icon: Icon(Icons.video_library_rounded)), // 스토리
+                  ],
+                ),
+              ),
+            ),
+          ];
+        },
+        body: TabBarView(
+          children: [
+            Builder(
+              builder: (context) {
+                return ProfileGridBuilder(
+                  isStoryTab: false,
+                  profileModel: profileModel,
+                  // 내부 SmartRefresher → CustomScrollView 안에 SliverOverlapInjector 필요
+                  injectorHandle:
+                      NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                return ProfileGridBuilder(
+                  isStoryTab: true,
+                  profileModel: profileModel,
+                  injectorHandle:
+                      NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                );
+              },
             ),
           ],
         ),
       ),
     );
   }
+}
+
+class _TabBarDelegate extends SliverPersistentHeaderDelegate {
+  final TabBar _tabBar;
+
+  _TabBarDelegate(this._tabBar);
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return Container(
+      color: Colors.white, // 배경색 필요시 지정
+      child: _tabBar,
+    );
+  }
+
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  bool shouldRebuild(_TabBarDelegate oldDelegate) => false;
 }
--- lib/ui/pages/holder/profile/widgets/profile_grid_builder.dart
+++ lib/ui/pages/holder/profile/widgets/profile_grid_builder.dart
@@ -5,90 +5,128 @@ import 'package:minigram/ui/pages/holder/profile/profile_vm.dart';
 import 'package:minigram/ui/pages/post/detail/post_detail_page.dart';
 import 'package:minigram/ui/pages/story/detail/story_detail_page.dart';
 import 'package:minigram/ui/widgets/m_grid_item.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class ProfileGridBuilder extends StatelessWidget {
+class ProfileGridBuilder extends StatefulWidget {
   final bool isStoryTab;
   final ProfileModel profileModel;
+
+  /// NestedScrollView와 연결하기 위해 SliverOverlapAbsorberHandle 전달
+  final SliverOverlapAbsorberHandle injectorHandle;
 
   const ProfileGridBuilder({
     super.key,
     this.isStoryTab = false,
     required this.profileModel,
+    required this.injectorHandle,
   });
 
   @override
-  Widget build(BuildContext context) {
-    final storyList = profileModel.storyListObject.storyList;
-    final postList = profileModel.postListObject.postList;
+  State<ProfileGridBuilder> createState() => _ProfileGridBuilderState();
+}
 
-    if (isStoryTab) {
-      return CustomScrollView(
-        slivers: [
-          SliverGrid(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                if (index == storyList.length) {
-                  return _AddBox();
-                } else {
-                  final item = storyList[index];
-                  return MGridItem(
-                    imageUrl: item.thumbnailUrl,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) =>
-                              StoryDetailPage(storyId: item.storyId),
-                        ),
-                      );
-                    },
-                  );
-                }
-              },
-              childCount: storyList.length + 1,
+class _ProfileGridBuilderState extends State<ProfileGridBuilder> {
+  final RefreshController _refreshController = RefreshController(
+    initialRefresh: false,
+  );
+
+  Future<void> _onLoading() async {
+    // TODO: 추가 데이터 로딩 로직 (API 호출 후 setState)
+    await Future.delayed(const Duration(milliseconds: 1500));
+    _refreshController.loadComplete();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final storyList = widget.profileModel.storyListObject.storyList;
+    final postList = widget.profileModel.postListObject.postList;
+
+    if (widget.isStoryTab) {
+      // 스토리 탭
+      return SmartRefresher(
+        controller: _refreshController,
+        enablePullDown: false,
+        enablePullUp: true,
+        onLoading: _onLoading,
+        child: CustomScrollView(
+          key: PageStorageKey<String>("storyTab"),
+          slivers: [
+            SliverOverlapInjector(handle: widget.injectorHandle),
+            SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  if (index == storyList.length) {
+                    return _AddBox();
+                  } else {
+                    final item = storyList[index];
+                    return MGridItem(
+                      imageUrl: item.thumbnailUrl,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) =>
+                                StoryDetailPage(storyId: item.storyId),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+                childCount: storyList.length + 1,
+              ),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: MSize.kRatio.aspect34,
+                crossAxisSpacing: MSize.kGap.xxxs,
+                mainAxisSpacing: MSize.kGap.xxxs,
+              ),
             ),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              childAspectRatio: MSize.kRatio.aspect34,
-              crossAxisSpacing: MSize.kGap.xxxs,
-              mainAxisSpacing: MSize.kGap.xxxs,
-            ),
-          ),
-        ],
+          ],
+        ),
       );
     } else {
-      return CustomScrollView(
-        slivers: [
-          SliverGrid(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                if (index == postList.length) {
-                  return _AddBox();
-                } else {
-                  final item = postList[index];
-                  return MGridItem(
-                    imageUrl: item.postImageUrl,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => PostDetailPage(postId: item.postId),
-                        ),
-                      );
-                    },
-                  );
-                }
-              },
-              childCount: postList.length + 1,
+      // 게시글 탭
+      return SmartRefresher(
+        controller: _refreshController,
+        enablePullDown: false,
+        enablePullUp: true,
+        onLoading: _onLoading,
+        child: CustomScrollView(
+          key: PageStorageKey<String>("postTab"),
+          slivers: [
+            SliverOverlapInjector(handle: widget.injectorHandle),
+            SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  if (index == postList.length) {
+                    return _AddBox();
+                  } else {
+                    final item = postList[index];
+                    return MGridItem(
+                      imageUrl: item.postImageUrl,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => PostDetailPage(postId: item.postId),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+                childCount: postList.length + 1,
+              ),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: MSize.kRatio.aspect34,
+                crossAxisSpacing: MSize.kGap.xxxs,
+                mainAxisSpacing: MSize.kGap.xxxs,
+              ),
             ),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              childAspectRatio: MSize.kRatio.aspect34,
-              crossAxisSpacing: MSize.kGap.xxxs,
-              mainAxisSpacing: MSize.kGap.xxxs,
-            ),
-          ),
-        ],
+          ],
+        ),
       );
     }
   }
