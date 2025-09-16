9c3f385e4450e0963db26815dce8b375c4cdc09d
hahamik <dlapdl0615@gmail.com>
2025-09-16 18:53:31 +0900

    [feat] 게시글 등록 (#54)

--- lib/ui/pages/holder/profile/widgets/profile_grid_builder.dart
+++ lib/ui/pages/holder/profile/widgets/profile_grid_builder.dart
@@ -12,138 +12,104 @@ import 'package:minigram/ui/pages/story/detail/story_detail_page.dart';
 import 'package:minigram/ui/pages/story/write/story_write_fm.dart';
 import 'package:minigram/ui/pages/story/write/story_write_page.dart';
 import 'package:minigram/ui/widgets/m_grid_item.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class ProfileGridBuilder extends ConsumerWidget {
   final bool isStoryTab;
-  final int userId;
-  final SliverOverlapAbsorberHandle injectorHandle;
+  final ProfileModel profileModel;
 
   const ProfileGridBuilder({
     super.key,
     this.isStoryTab = false,
-    required this.userId,
-    required this.injectorHandle,
+    required this.profileModel,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final profileModel = ref.watch(profileProvider(userId));
-    final profileVM = ref.read(profileProvider(userId).notifier);
-
-    if (profileModel == null) {
-      return const Center(child: CircularProgressIndicator());
-    }
-
     final storyList = profileModel.storyListObject.storyList;
     final postList = profileModel.postListObject.postList;
 
     if (isStoryTab) {
-      return SmartRefresher(
-        controller: profileVM.storyScrollController,
-        enablePullDown: true,
-        enablePullUp: true,
-        onRefresh: () => profileVM.init(userId: userId),
-        onLoading: () => profileVM.nextStoryList(userId: userId),
-        child: CustomScrollView(
-          key: const PageStorageKey<String>("storyTab"),
-          slivers: [
-            SliverOverlapInjector(handle: injectorHandle),
-            SliverGrid(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  if (index == storyList.length) {
-                    return _AddBox(context);
-                  } else {
-                    final item = storyList[index];
-                    return MGridItem(
-                      imageUrl: item.thumbnailUrl,
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) =>
-                                StoryDetailPage(storyId: item.storyId),
-                          ),
-                        );
-                      },
-                    );
-                  }
-                },
-                childCount: storyList.length + 1,
-              ),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                childAspectRatio: MSize.kRatio.aspect34,
-                crossAxisSpacing: MSize.kGap.xxxs,
-                mainAxisSpacing: MSize.kGap.xxxs,
-              ),
+      return CustomScrollView(
+        slivers: [
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                if (index == storyList.length) {
+                  return _AddBox(context, ref);
+                } else {
+                  final item = storyList[index];
+                  return MGridItem(
+                    imageUrl: item.thumbnailUrl,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => StoryDetailPage(storyId: item.storyId),
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+              childCount: storyList.length + 1,
             ),
-          ],
-        ),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: MSize.kRatio.aspect34,
+              crossAxisSpacing: MSize.kGap.xxxs,
+              mainAxisSpacing: MSize.kGap.xxxs,
+            ),
+          ),
+        ],
       );
     } else {
-      return SmartRefresher(
-        controller: profileVM.postScrollController,
-        enablePullDown: true,
-        enablePullUp: true,
-        onRefresh: () => profileVM.init(userId: userId),
-        onLoading: () => profileVM.nextPostList(userId: userId),
-        child: CustomScrollView(
-          key: const PageStorageKey<String>("postTab"),
-          slivers: [
-            SliverOverlapInjector(handle: injectorHandle),
-            SliverGrid(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  if (index == postList.length) {
-                    return _AddBox(context);
-                  } else {
-                    final item = postList[index];
-                    return MGridItem(
-                      imageUrl: item.postImageUrl,
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => PostDetailPage(postId: item.postId),
-                          ),
-                        );
-                      },
-                    );
-                  }
-                },
-                childCount: postList.length + 1,
-              ),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                childAspectRatio: MSize.kRatio.aspect34,
-                crossAxisSpacing: MSize.kGap.xxxs,
-                mainAxisSpacing: MSize.kGap.xxxs,
-              ),
+      return CustomScrollView(
+        slivers: [
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                if (index == postList.length) {
+                  return _AddBox(context, ref);
+                } else {
+                  final item = postList[index];
+                  return MGridItem(
+                    imageUrl: item.postImageUrl,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => PostDetailPage(postId: item.postId),
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+              childCount: postList.length + 1,
             ),
-          ],
-        ),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: MSize.kRatio.aspect34,
+              crossAxisSpacing: MSize.kGap.xxxs,
+              mainAxisSpacing: MSize.kGap.xxxs,
+            ),
+          ),
+        ],
       );
     }
   }
 
-  Material _AddBox(BuildContext context) {
-    final ImagePicker _picker = ImagePicker();
+  Material _AddBox(BuildContext context, WidgetRef ref) {
+    final ImagePicker picker = ImagePicker();
 
-    Future<void> _pickVideo(BuildContext context) async {
-      final XFile? video = await _picker.pickVideo(
+    Future<void> _pickVideo() async {
+      final XFile? video = await picker.pickVideo(
         source: ImageSource.gallery,
         maxDuration: const Duration(seconds: 60),
       );
 
       if (video != null) {
-        print("선택한 영상 경로: ${video.path}");
-
-        final container = ProviderScope.containerOf(context, listen: false);
-        container
-            .read(storyWriteProvider.notifier)
-            .uploadStory(File(video.path));
+        ref.read(storyWriteProvider.notifier).uploadStory(File(video.path));
 
         Navigator.push(
           context,
@@ -155,7 +121,7 @@ class ProfileGridBuilder extends ConsumerWidget {
     }
 
     Future<void> _pickImages() async {
-      final List<XFile> images = await _picker.pickMultiImage(
+      final List<XFile> images = await picker.pickMultiImage(
         imageQuality: 85,
         limit: 10,
       );
@@ -176,11 +142,10 @@ class ProfileGridBuilder extends ConsumerWidget {
       color: MColor.kBackGround.lightGray,
       child: InkWell(
         onTap: () async {
-          print("추가하기 클릭됨");
           if (isStoryTab) {
-            await _pickVideo(context); // 스토리 탭이면 비디오 선택
+            await _pickVideo();
           } else {
-            await _pickImages(); // 게시글 탭이면 이미지 선택
+            await _pickImages();
           }
         },
         child: const Center(
