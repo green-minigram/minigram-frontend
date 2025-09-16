3f8e0d54861eb7deef2e7d80858ad5ace6bd0f68
jjack-1 <qazwsx0088@gmail.com>
2025-09-16 17:16:02 +0900

    [feat] 홈 피드 팔로잉 기능 (#53)

--- lib/data/model/user.dart
+++ lib/data/model/user.dart
@@ -5,6 +5,7 @@ class User {
   String? roles;
   String? accessToken;
   bool? isFollowing;
+  bool? showFollowButton;
 
   User({
     required this.userId,
@@ -13,6 +14,7 @@ class User {
     this.profileImageUrl,
     this.accessToken,
     this.isFollowing,
+    this.showFollowButton,
   });
 
   User.fromMap(Map<String, dynamic> data)
@@ -20,11 +22,34 @@ class User {
       username = data['username'],
       roles = data['roles'] ?? "",
       profileImageUrl = data['profileImageUrl'] ?? "",
-      isFollowing = data['isFollowing'] ?? false,
+      isFollowing = data['isFollowing'] as bool?,
+      showFollowButton = (data['isFollowing'] == null)
+          ? null
+          : !((data['isFollowing'] as bool)),
       accessToken = data['accessToken'] ?? "";
 
   @override
   String toString() {
-    return 'User{userId: $userId, username: $username, profileImageUrl: $profileImageUrl,roles: $roles, accessToken: $accessToken}';
+    return 'User(userId: $userId, username: $username, roles: $roles, profileImageUrl: $profileImageUrl, accessToken: $accessToken, isFollowing: $isFollowing, initialFollowingState: $showFollowButton)';
+  }
+
+  User copyWith({
+    int? userId,
+    String? username,
+    String? profileImageUrl,
+    String? roles,
+    String? accessToken,
+    bool? isFollowing,
+    bool? showFollowButton,
+  }) {
+    return User(
+      userId: userId ?? this.userId,
+      username: username ?? this.username,
+      profileImageUrl: profileImageUrl ?? this.profileImageUrl,
+      roles: roles ?? this.roles,
+      accessToken: accessToken ?? this.accessToken,
+      isFollowing: isFollowing ?? this.isFollowing,
+      showFollowButton: showFollowButton ?? this.showFollowButton,
+    );
   }
 }
