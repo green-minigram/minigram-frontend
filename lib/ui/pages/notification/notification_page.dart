f6a021b1a29af9b6dd7c770af6ed73b09ec60d82
jjack-1 <qazwsx0088@gmail.com>
2025-09-19 11:54:47 +0900

    시연 완료

--- lib/ui/pages/notification/notification_page.dart
+++ lib/ui/pages/notification/notification_page.dart
@@ -8,96 +8,81 @@ class NotificationPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final notifications = {
       "notifications": {
-        "likes": [
-          {
-            "user": {
-              "userId": 1,
-              "username": "busanlottgiants",
-              "name": "롯데 자이언츠",
-              "profileImageUrl": "https://cdn.pixabay.com/photo/2025/08/12/08/49/cat-9769723_1280.jpg",
-              "isFollowing": false,
-              "followingId": null,
-            },
-            "post": {
-              "postId": 101,
-              "postImageUrl": "https://cdn.pixabay.com/photo/2025/08/21/09/51/rouen-cathedral-9787080_1280.jpg",
-            },
-            "createdAt": "2025-08-30 10:15:00",
-          },
-          {
-            "user": {
-              "userId": 2,
-              "username": "user123",
-              "name": "유저123",
-              "profileImageUrl": "https://cdn.pixabay.com/photo/2025/08/12/08/49/cat-9769723_1280.jpg",
-              "isFollowing": false,
-              "followingId": null,
-            },
-            "post": {
-              "postId": 102,
-              "postImageUrl": "https://cdn.pixabay.com/photo/2025/08/21/09/51/rouen-cathedral-9787080_1280.jpg",
-            },
-            "createdAt": "2025-08-30 10:15:00",
-          },
-        ],
+        // "likes": [
+        //   {
+        //     "user": {
+        //       "userId": 3,
+        //       "username": "cos",
+        //       "name": "코스",
+        //       "profileImageUrl": "https://picsum.photos/seed/cos/200",
+        //       "isFollowing": true,
+        //       "followingId": 3,
+        //     },
+        //     "post": {
+        //       "postId": 26,
+        //       "postImageUrl": "https://cdn.pixabay.com/photo/2024/11/08/12/57/cat-9183327_1280.jpg",
+        //     },
+        //     "createdAt": "2025-09-17 16:15:00",
+        //   },
+        // ],
         "comments": [
           {
             "user": {
               "userId": 3,
-              "username": "kimseryuk",
-              "name": "김세륙",
-              "profileImageUrl": "https://cdn.pixabay.com/photo/2025/08/12/08/49/cat-9769723_1280.jpg",
+              "username": "cos",
+              "name": "코스",
+              "profileImageUrl": "https://picsum.photos/seed/cos/200",
               "isFollowing": true,
               "followingId": 3,
             },
             "post": {
-              "postId": 103,
-              "postImageUrl": "https://cdn.pixabay.com/photo/2025/08/21/09/51/rouen-cathedral-9787080_1280.jpg",
+              "postId": 26,
+              "postImageUrl": "https://cdn.pixabay.com/photo/2024/11/08/12/57/cat-9183327_1280.jpg",
             },
-            "comment": "와 진짜 멋지네요!",
-            "createdAt": "2025-08-30 10:15:00",
+            "comment": "고양이 이쁘네요",
+            "createdAt": "2025-09-17 17:15:00",
           },
-          {
-            "user": {
-              "userId": 4,
-              "username": "hannah",
-              "name": "Hannah",
-              "profileImageUrl": "https://cdn.pixabay.com/photo/2025/08/12/08/49/cat-9769723_1280.jpg",
-              "isFollowing": false,
-              "followingId": null,
-            },
-            "post": {
-              "postId": 104,
-              "postImageUrl": "https://cdn.pixabay.com/photo/2025/08/21/09/51/rouen-cathedral-9787080_1280.jpg",
-            },
-            "comment": "이건 어디서 찍으신 건가요?",
-            "createdAt": "2025-08-30 10:15:00",
-          },
+          // {
+          //   "user": {
+          //     "userId": 4,
+          //     "username": "hannah",
+          //     "name": "Hannah",
+          //     "profileImageUrl": "https://cdn.pixabay.com/photo/2025/08/12/08/49/cat-9769723_1280.jpg",
+          //     "isFollowing": false,
+          //     "followingId": null,
+          //   },
+          //   "post": {
+          //     "postId": 104,
+          //     "postImageUrl": "https://cdn.pixabay.com/photo/2025/08/21/09/51/rouen-cathedral-9787080_1280.jpg",
+          //   },
+          //   "comment": "이건 어디서 찍으신 건가요?",
+          //   "createdAt": "2025-08-30 10:15:00",
+          // },
         ],
-        "follows": [
-          {
-            "user": {
-              "userId": 5,
-              "username": "kwonohyeon",
-              "name": "권오현",
-              "profileImageUrl": "https://cdn.pixabay.com/photo/2025/08/12/08/49/cat-9769723_1280.jpg",
-              "isFollowing": true,
-              "followingId": 5,
-            },
-            "createdAt": "2025-08-30 10:15:00",
-          },
-          {
-            "user": {
-              "userId": 6,
-              "username": "new_fan",
-              "name": "new fan",
-              "profileImageUrl": "https://cdn.pixabay.com/photo/2025/08/12/08/49/cat-9769723_1280.jpg",
-              "isFollowing": false,
-              "followingId": null,
-            },
-            "createdAt": "2025-08-30 10:15:00",
-          },
-        ],
+        // "follows": [
+        //   {
+        //     "user": {
+        //       "userId": 5,
+        //       "username": "kwonohyeon",
+        //       "name": "권오현",
+        //       "profileImageUrl": "https://cdn.pixabay.com/photo/2025/08/12/08/49/cat-9769723_1280.jpg",
+        //       "isFollowing": true,
+        //       "followingId": 5,
+        //     },
+        //     "createdAt": "2025-08-30 10:15:00",
+        //   },
+        //   {
+        //     "user": {
+        //       "userId": 6,
+        //       "username": "new_fan",
+        //       "name": "new fan",
+        //       "profileImageUrl": "https://cdn.pixabay.com/photo/2025/08/12/08/49/cat-9769723_1280.jpg",
+        //       "isFollowing": false,
+        //       "followingId": null,
+        //     },
+        //     "createdAt": "2025-08-30 10:15:00",
+        //   },
+        // ],
       },
     };
     return Scaffold(
